6782652fe7eb789eaf748455ea81a8462c7f9a1c
Recep Şen <78692387+recep-sen@users.noreply.github.com>
2022-08-22 19:23:25 +0300

    Basic player input and some test materials

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 3f;
+    
+    private Vector2 movementVector = Vector2.zero;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movementVector * movementSpeed;
+    }
+    public void GetMovementInput(CallbackContext context)
+    {
+        movementVector = context.ReadValue<Vector2>();
+    }
+
+}
